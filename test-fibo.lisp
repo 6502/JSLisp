377f27377fb4fbb99929bbad67af1bb148526274
Andrea Griffini <agriff@tin.it>
2011-08-06 17:39:25 +0200

    added resolved version and memoizer

--- test-fibo.lisp
+++ test-fibo.lisp
@@ -13,3 +13,25 @@
             ,@body
             (- (clock) ,start))))
 (time (test-fibo 30))
+
+(defun fibo (n)
+    ; f(n) = f(n-1) + f(n-2) = a + b
+    ; f(n+1) = f(n) + f(n-1) = a' + b'
+    ; a' = a+b
+    ; b' = a
+    (labels ((ff (a b count)
+                (if (= count n)
+                    (+ a b)
+                    (ff (+ a b) a (1+ count)))))
+        (if (< n 2)
+            1
+            (ff 1 1 2))))
+
+(defun memoized (f)
+  (let ((cache (js-object)))
+    (lambda (&rest x)
+      (let ((cached-result (aref cache x)))
+        (if (= cached-result undefined)
+            (setf (aref cache x)
+                  (apply f x))
+            cached-result)))))
