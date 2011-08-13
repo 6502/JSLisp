d9956681916519dbf41a5f25e3510a50c400fd93
Andrea Griffini <agriff@tin.it>
2011-08-13 21:31:41 +0200

    wip gui module

--- gui.lisp
+++ gui.lisp
@@ -57,6 +57,114 @@
                  (hide cover))
     (show cover)))
 
+(defstruct layout-node
+  (class 1) (weight 100)   ;; Weighted distribution when in the same class
+  (min 0) (max 1000000)    ;; Minimum and maximum space
+  (buddy null)             ;; Someone to inform about the size
+  (algorithm :H)           ;; Algorithm for children
+  (border 0)               ;; Fixed border around space for children
+  (spacing 0)              ;; Fixed space between children
+  (children (list)))       ;; List of children nodes (if any)
+
+(defun set-coords (node x0 y0 x1 y1)
+  (let* ((children (layout-node-children node))
+         (nchild (length children))
+         (border (layout-node-border node))
+         (spacing (layout-node-spacing node))
+         (algo (layout-node-algorithm node)))
+    (when (layout-node-buddy node)
+      (funcall (layout-node-buddy node) x0 y0 x1 y1))
+    (when (> nchild 0)
+      (let ((assigned (map #'layout-node-min children))
+            (active (filter (lambda (i) (< (layout-node-min (aref children i))
+                                           (layout-node-max (aref children i))))
+                            (range nchild)))
+            (avail (- (if (= algo :H)
+                          (- x1 x0)
+                          (- y1 y0))
+                      (* 2 border)
+                      (* (1- nchild) spacing))))
+        (decf avail (reduce #'+ assigned))
+        (do () ((or (zerop (length active))
+                    (<= avail 0.0001))
+                  (if (= algo :H)
+                      (let ((x (+ x0 border))
+                            (ya (+ y0 border))
+                            (yb (- y1 border)))
+                        (dotimes (i nchild)
+                          (set-coords (aref children i)
+                                      x ya
+                                      (+ x (aref assigned i)) yb)
+                          (incf x (+ (aref assigned i) spacing))))
+                      (let ((y (+ y0 border))
+                            (xa (+ x0 border))
+                            (xb (- x1 border)))
+                        (dotimes (i nchild)
+                          (set-coords (aref children i)
+                                      xa y
+                                      xb (+ y (aref assigned i)))
+                          (incf y (+ (aref assigned i) spacing))))))
+          ;;
+          ;; Algorithm:
+          ;;
+          ;; First select the highest priority class among all active
+          ;; nodes, then try to distribute the available space in
+          ;; proportion to weights but not exceeding the maximum for a
+          ;; given node.  Finally remove saturated nodes from the
+          ;; active list.
+          ;;
+          ;; At every step at least one node is saturated, or all
+          ;; available space is distributed.
+          ;;
+          ;; We're not going to loop forever.
+          ;;
+          (let* ((minclass (min (map (lambda (i)
+                                       (layout-node-class (aref children i)))
+                                     active)))
+                 (selected (filter (lambda (i) (= (layout-node-class (aref children i))
+                                                  minclass))
+                                   active))
+                 (selected-nodes (map (lambda (i) (aref children i))
+                                      selected))
+                 (total-weight (reduce #'+ (map #'layout-node-weight selected-nodes)))
+                 (share (/ avail total-weight)))
+            (dolist (i selected)
+              (let* ((n (aref children i))
+                     (quota (min (list (- (layout-node-max n) (aref assigned i))
+                                       (* share (layout-node-weight n))))))
+                (decf avail quota)
+                (incf (aref assigned i) quota)))
+            (setf active (filter (lambda (i)
+                                   (< (+ (aref assigned i) 0.0001)
+                                      (layout-node-max (aref children i))))
+                                 active))))))))
+
+(defmacro deflayout (type)
+  (let ((x0 (gensym))
+        (y0 (gensym))
+        (x1 (gensym))
+        (y1 (gensym)))
+    `(progn
+       (defmacro ,type (&rest args)
+         (do ((i 0 (+ i 2)))
+             ((or (= i (length args))
+                  (not (symbolp (aref args i)))
+                  (/= (aref (symbol-name (aref args i)) 0) ":"))
+                `(make-layout-node :algorithm ,,type
+                                   ,@(slice args 0 i)
+                                   :children (list ,@(slice args i))))))
+       (defmacro ,(intern (+ (symbol-name type) "div")) (div &rest args)
+         `(,,type :buddy (lambda (,',x0 ,',y0 ,',x1 ,',y1)
+                           (set-style ,div
+                                      px/left ,',x0
+                                      px/top ,',y0
+                                      px/width (- ,',x1 ,',x0)
+                                      px/height (- ,',y1 ,',y0)))
+                  ,@args)))))
+
+(deflayout :H)
+(deflayout :V)
+
 (defun window (x0 y0 w h title)
   (let ((window (create-element "div")))
     (set-style window
