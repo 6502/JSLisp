cda036a4a81e6421c0c3229f62842a2a374f0fca
Andrea Griffini <agriff@tin.it>
2012-04-15 11:16:32 +0200

    revised import and objects support

--- objects.lisp
+++ objects.lisp
@@ -1,23 +1,29 @@
 (defmacro defmethod (name args test &rest body)
-  (setf name (module-symbol name))
-  (let ((mvar (intern ~"*{name}-methods*"))
-        (avar (intern ~"*{name}-arglist*")))
-    (unless (symbol-value mvar)
-      (setf (symbol-value mvar) `((true (error ,~"No matching method [{name}]")))))
-    (let ((sa (str-value args false)))
-      (if (symbol-value avar)
-          (unless (= sa (symbol-value avar))
-            (error ~"Method [{name}] arglist mismatch"))
-          (setf (symbol-value avar) sa)))
-    (push `(,test ,@body) (symbol-value mvar))
-    `(defun ,name ,args
-       (cond
-         ,@(reverse (symbol-value mvar))))))
+  "Defines a conditioned implementation of a function; when [test] doesn't match the previous implementation is called instead"
+  (let ((of '#.(gensym)))
+    `(progn
+       (unless (symbol-function ',name)
+         (setf (symbol-function ',name)
+               (lambda (&rest args)
+                 (error ,~"No matching method [{name}]"))))
+       (setf (symbol-function ',name)
+             (let ((,of (symbol-function ',name)))
+               (lambda ,args
+                 (if ,test
+                     (progn ,@body)
+                     (funcall ,of ,@args))))))))
 
-(defmethod fields (x) true null)
-(defmethod class (x) true null)
-(defmethod make-instance (class &rest args) true
-  (error "Undefined class"))
+(defun fields (x)
+  "Returns the fields associated to object instance [x]"
+  (and (list? x)
+       (list? (first x))
+       (slice (first x) 1)))
+
+(defun class (x)
+  "Returns the class [name] associated to object instance [x]"
+  (and (list? x)
+       (list? (first x))
+       (first (first x))))
 
 (defmacro defobject (name fields)
   "Defines an object with given [name] and [fields]
@@ -25,10 +31,7 @@ Each field can be either a symbol for the field name or a list for field name an
 After the macro invocation the following functions/macros/methods are defined:
 function [(<name> f1 f2 ... fn)] Creates an instance
 function [(make-<name> :f1 f1 :f2 f2 ... :fn fn)] Creates an instance
-method [(make-instance '<name> &rest args)] Indirect invocation of [(<name> ...)]
 function [(<name>? x)] True if and only if is an instance created by one of the above
-method [(fields x)] Returns a list of the field names of the instance [x]
-method [(class x)] Returns the [name] associated with an instance
 method [(<field> x)] field getter
 method [(set-<field> x value)] field setter
 method [(inc-<field> x inc)] field increment
@@ -39,31 +42,26 @@ macro [(with-<name>* x &rest body)] evaluates body after dynamically binding r/w
   (setf name (module-symbol name))
   (unless (list? fields)
     (error "Syntax is (defobject name (field1 field2 ... fieldn))"))
-  (let ((fnames (map (lambda (x)
-                       (cond
-                         ((symbol? x) x)
-                         ((and (list? x)
-                               (= (length x) 2)
-                               (symbol? (first x)))
-                          (first x))
-                         (true
-                          (error "Each field must be either a symbol or a symbol/default-value pair"))))
-                     fields)))
+  (let* ((fnames (map (lambda (x)
+                        (cond
+                          ((symbol? x) x)
+                          ((and (list? x)
+                                (= (length x) 2)
+                                (symbol? (first x)))
+                           (first x))
+                          (true
+                           (error "Each field must be either a symbol or a symbol/default-value pair"))))
+                      fields))
+         (id `(,name ,@fnames)))
     `(progn
        (defun ,name (&optional ,@fields)
          ,~"Builds an instance of {name}"
-         (list ',fnames ,@fnames))
+         (list ',id ,@fnames))
        (defun ,(intern ~"make-{name}") (&key ,@fields)
          ,~"Builds an instance of {name}"
-         (list ',fnames ,@fnames))
-       (defmethod make-instance (class &rest args) (= class ',name)
-         (apply #',name args))
+         (list ',id ,@fnames))
        (defun ,(intern ~"{name}?") (x)
-         (and (list? x) (= (first x) ',fnames)))
-       (defmethod fields (x) (,(intern ~"{name}?") x)
-         ',fields)
-       (defmethod class (x) (,(intern ~"{name}?") x)
-         ',name)
+         (and (list? x) (= (first x) ',id)))
        ,@(map (lambda (f)
                 (let ((ix (1+ (index f fnames)))
                       (set (intern ~"set-{f}"))
@@ -110,5 +108,4 @@ macro [(with-<name>* x &rest body)] evaluates body after dynamically binding r/w
                     ,@body)))))
        ',name)))
 
-(export defobject defmethod
-        field class make-instance)
+(export defobject defmethod class fields)
