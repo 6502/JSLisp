04484ff48215f4d9a5a0c4e257811073fac84df1
Andrea Griffini <agriff@tin.it>
2012-09-12 14:04:57 +0200

    character encoding in html-deploy

--- deploy-html.lisp
+++ deploy-html.lisp
@@ -1,3 +1,3 @@
 (load (get-file "deploy.lisp"))
-(setf *deploy-prefix* "<doctype HTML><html><body><script>\n")
+(setf *deploy-prefix* "<doctype HTML><html><head><meta http-equiv=\"Content-Type\" content = \"text/html; charset=utf-8\"></head><body><script>\n")
 (setf *deploy-suffix* "\n</script></body></html>")
