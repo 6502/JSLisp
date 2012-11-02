5a98e0e95bb6e60f66279b56c762dcaec8f0409f
Andrea Griffini <agriff@tin.it>
2012-11-02 23:14:49 +0100

    Heap module rewrite

--- heap.lisp
+++ heap.lisp
@@ -1,141 +1,128 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;                                                                          ;;;
-;;;  Copyright (c) 2011 by Andrea Griffini                                   ;;;
-;;;                                                                          ;;;
-;;;  Permission is hereby granted, free of charge, to any person obtaining   ;;;
-;;;  a copy of this software and associated documentation files (the         ;;;
-;;;  "Software"), to deal in the Software without restriction, including     ;;;
-;;;  without limitation the rights to use, copy, modify, merge, publish,     ;;;
-;;;  distribute, sublicense, and/or sell copies of the Software, and to      ;;;
-;;;  permit persons to whom the Software is furnished to do so, subject to   ;;;
-;;;  the following conditions:                                               ;;;
-;;;                                                                          ;;;
-;;;  The above copyright notice and this permission notice shall be          ;;;
-;;;  included in all copies or substantial portions of the Software.         ;;;
-;;;                                                                          ;;;
-;;;  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,         ;;;
-;;;  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF      ;;;
-;;;  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                   ;;;
-;;;  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE  ;;;
-;;;  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION  ;;;
-;;;  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION   ;;;
-;;;  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.         ;;;
-;;;                                                                          ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defmacro/f heap-parent (x)
-  "Returns the index of the parent of the element with index x"
-  `(js-code ,(+ "((" (js-compile x) "-1)>>1)")))
-
-(defmacro/f heap-first-child (x)
-  "Returns the index of the first child of the element with index x"
-  `(js-code ,(+ "(1+(" (js-compile x) "<<1))")))
-
-(defmacro/f heap-second-child (x)
-  "Returns the index of the second child of the element with index x"
-  `(js-code ,(+ "(2+(" (js-compile x) "<<1))")))
-
-(defstruct heap
-  data before-than index-tracking)
-
-(defun heap (&optional (before-than #'<=) index-tracking)
-  "Creates an heap object
-   - before-than is a callable accepting two parms that must
-     return true if first element can precede second.
-   - index-tracking is a callable that if present will be called with the
-     element and the index to track its position in the heap."
-  (make-heap data: (list)
-             before-than: before-than
-             index-tracking: index-tracking))
-
-(defmacro/f heap-length (heap)
-  "Returns the number of elements contained in the heap"
-  `(length (heap-data ,heap)))
-
-(defmacro/f heap-aref (heap index)
-  "Returns an element from the heap given the index"
-  `(aref (heap-data ,heap) ,index))
-
 (defun heap-fix (heap index)
-  "Adjustes the position of item index in the heap assuming all other elements are
-currently are satisfying the heap invariant. Returns true if elements have been moved."
-  (let ((data (heap-data heap))
-        (bt (heap-before-than heap))
-        (sz (heap-length heap))
-        (it (heap-index-tracking heap))
-        (moved false))
-    ;; Bubble up if lighter
-    (do ((parent (heap-parent index)))
-        ((or (= index 0)
-             (funcall bt (aref data parent) (aref data index))))
-      (setf moved true)
-      (let ((x (aref data index))
-            (y (aref data parent)))
-        (setf (aref data index) y)
-        (setf (aref data parent) x)
-        (when it
-          (funcall it y index)
-          (funcall it x parent))
-        (setf index parent)
-        (setf parent (heap-parent parent))))
-    ;; Sink if heavier
-    (do ((child (heap-first-child index)))
-        ((or (>= child sz)
-             (and (funcall bt (aref data index) (aref data child))
-                  (or (>= (1+ child) sz)
-                      (funcall bt (aref data index) (aref data (1+ child)))))))
-      (setf moved true)
-      (when (and (< (1+ child) sz)
-                 (not (funcall bt (aref data child) (aref data (1+ child)))))
-        (incf child))
-      (let ((x (aref data index))
-            (y (aref data child)))
-        (setf (aref data index y))
-        (setf (aref data child x))
-        (when it
-          (funcall it y index)
-          (funcall it x child))
-        (setf index child)
-        (setf child (heap-first-child child))))
-    moved))
+  "Eventually fixes the position of element [index] in the specified [heap] \
+   if this is needed to maintain heap invariant.
+   The function returns [true] if one or more changes were needed. The heap \
+   invariant function is specified when creating the heap object with {{make-heap}}.
+   This function must be called if the logical value of an element is changed \
+   and this influences its positioning in respect to the heap invariant."
+  (macrolet ((parent (x) `(ash (1- ,x) -1))
+             (child (x) `(1+ (ash ,x 1)))
+             (swap-with (x)
+               `(progn
+                  (setf moved true)
+                  (swap (aref heap ,x) (aref heap index))
+                  (when heap.tracking
+                    (heap.tracking (aref heap ,x) ,x)
+                    (heap.tracking (aref heap index) index))
+                  (setf index ,x))))
+    (let ((moved false))
+      (do ((parent (parent index) (parent index)))
+          ((or (= index 0) (heap.lte (aref heap parent) (aref heap index))))
+        (swap-with parent))
+      (do ((child (child index) (child index)))
+          ((and (or (>= child (length heap))
+                    (heap.lte (aref heap index) (aref heap child)))
+                (or (>= (incf child) (length heap))
+                    (heap.lte (aref heap index) (aref heap child)))))
+        (swap-with child))
+      moved)))
+
+(defun heap-pop (heap &optional (index 0))
+  "Removes and returns the specified element from the [heap], moving other \
+   elements if this is required to maintain the heap invariant."
+  (let ((x (aref heap index)))
+    (when heap.tracking (heap.tracking x null))
+    (let ((y (pop heap)))
+      (when (> (length heap) 0)
+        (setf (aref heap index) y)
+        (heap.tracking (heap.tracking y index))
+        (heap-fix heap index)))
+    x))
 
 (defun heap-push (x heap)
-  "Adds a new element to an heap, maintaining the heap invariant"
-  (push x (heap-data heap))
-  (when (heap-index-tracking heap)
-    (funcall (heap-index-tracking heap) x (1- (heap-length heap))))
-  (heap-fix heap (1- (heap-length heap))))
+  "Adds the specified element to the heap, moving eventually other elements \
+   if this is required to maintain the heap invariant."
+  (push x heap)
+  (heap.tracking (heap.tracking x (1- (length heap))))
+  (heap-fix heap (1- (length heap))))
 
-(defmacro/f heap-top (heap)
-  "Returns the current top element of an heap, without removing it"
-  `(first (heap-data ,heap)))
+(defun make-heap (data &optional lte tracking)
+  "Transforms the specified list [data] into an heap data structure using \
+   the specified heap invariant and tracking function.
+   The heap invariant function [lte] takes two elements and must return [true] \
+   if it's valid for the first element to appear before than the second.
+   When not specified the [lte] function defaults to [#'<].
+   The tracking function if present will be called passing an element \
+   and the index in which it's placed inside the heap. Every time an \
+   element needs to be moved around to maintain the heap invariant the tracking \
+   function will be called to inform the element. The function is also called \
+   on all elements every time [make-heap] is called.
+   The function [make-heap] can also accept an object that is already \
+   an heap and in this case the invariant is re-established by sorting the \
+   elements. When calling [make-heap] on an object that is already an heap \
+   the current [lte] and [tracking] functions are maintained unless new values \
+   are specified in the [make-heap] call."
+  (setf lte (or lte data.lte #'<=))
+  (setf tracking (or tracking data.tracking))
+  (setf data.lte lte)
+  (setf data.tracking tracking)
+  (nsort data (lambda (a b) (not (funcall lte b a))))
+  (when tracking
+    (enumerate (i x data)
+      (funcall tracking x i)))
+  data)
 
-(defun heap-pop (heap)
-  "Removes the top element from an heap and returns it"
-  (let ((result (heap-top heap))
-        (x (pop (heap-data heap)))
-        (it (heap-index-tracking heap)))
-    (when it (funcall it result null))
-    (when (> (heap-length heap) 0)
-      (setf (aref (heap-data heap) 0) x)
-      (when it (funcall it x 0))
-      (heap-fix heap 0))
-    result))
+(defun heap-check (heap &optional check-tracking)
+  "Checks the internal consinstency of an heap object
+   The check is done by verifying the heap invariant and by
+   optionally calling a tracking check function on all elements.
+   The check-tracking function is present should take an element and its current \
+   index, returning [false] in case the index is not what is supposed to be."
+  (enumerate (index x (length heap))
+    (when check-tracking
+      (unless (funcall check-tracking x index)
+        (error ~"Invalid tracking value at index {index}")))
+    (let ((parent (ash (1- index) -1))
+          (child1 (+ 1 (ash index 1)))
+          (child2 (+ 1 (ash index 2))))
+      (unless (or (= index 0)
+                  (heap.lte (aref heap parent) (aref heap index)))
+        (error ~"Invalid parent invariant at index {index}"))
+      (unless (or (>= child1 (length heap))
+                  (heap.lte (aref heap index) (aref heap child1)))
+        (error ~"Invalid child-1 invariant at index {index}"))
+      (unless (or (>= child2 (length heap))
+                  (heap.lte (aref heap index) (aref heap child2)))
+        (error ~"Invalid child-2 invariant at index {index}")))))
 
-(defun heap-check (heap)
-  "Checks internal heap consistency"
-  (let ((data (heap-data heap))
-        (bt (heap-before-than heap))
-        (sz (heap-length heap)))
-    (dotimes (i sz)
-      (let ((c1 (heap-first-child i))
-            (c2 (heap-second-child i)))
-        (when (or (and (< c1 sz) (not (funcall bt (aref data i) (aref data c1))))
-                  (and (< c2 sz) (not (funcall bt (aref data i) (aref data c2)))))
-          (error "Invalid heap data"))))))
+(defun test-module ()
+  "Module test funtion. Exercises all funcions in the module by doing 100,000+ \
+   random operations and checks."
+  (repeat 100
+    (let ((data (map (lambda (i)
+                       (list i (random-int 100)))
+                     (range 10))))
+      (labels ((tracking (x i)
+                 (setf (first x) i))
+               (check-tracking (x i)
+                 (= (first x) i))
+               (lte (a b)
+                 (<= (second a) (second b))))
+        (make-heap data #'lte #'tracking)
+        (heap-check data #'check-tracking)
+        (repeat 1000
+          (when (and (> (length data) 0)
+                     (= (random-int 2) 1))
+            (heap-pop data)
+            (heap-check data #'check-tracking))
+          (when (= (random-int 2) 1)
+            (heap-push (random-int 100) data)
+            (heap-check data #'check-tracking))
+          (when (and (> (length data) 0)
+                     (= (random-int 100) 1))
+            (let ((ix (random-int (length data))))
+              (setf (second (aref data ix)) (random-int 100))
+              (make-heap data)
+              (heap-check data #'check-tracking))))))))
 
-(export heap
-        heap-length heap-push heap-pop heap-top
-        heap-fix heap-check)
+(export make-heap heap-check heap-push heap-pop heap-fix test-module)
