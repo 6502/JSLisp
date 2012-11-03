f89b8ad0e6622fb96140ac313c60779a645a18ad
Andrea Griffini <agriff@tin.it>
2012-11-03 11:13:55 +0100

    revised pathfinder module

--- pathfinder.lisp
+++ pathfinder.lisp
@@ -1,99 +1,102 @@
 (import * from heap)
 
-(defstruct state-info
-  state
-  total-cost
-  previous-state
-  heap-index)
+(defobject info
+    (state
+     cost
+     prev
+     index))
 
-(defun find-path (start-states next goal)
-  "Path-finding algorithm:
-      start-states ... list of starting states
-      next ........... function that given a state returns a list of (neighbor cost) pairs
-      goal ........... a function that returns true if the passed state is a goal state
-   The function returns either a minimal cost path of states or null if no such a path exists"
+(defun find-path (start next goal)
+  "Path-finding algorithm.
+   [start] is a list of starting states, [next] is a function that given a state \
+   returns a list of [(neighbor cost)] pairs and [goal] is a function that returns \
+   [true] if the passed state is a goal state.
+   The function returns eiter a minimal cost path of states to a goal state or \
+   [null] if no such a path exists."
   (let ((seen #())
-        (heap (heap (lambda (a b) (<= (state-info-total-cost a) (state-info-total-cost b)))
-                    (lambda (x index) (setf (state-info-heap-index x) index)))))
-    (dolist (x start-states)
-      (let ((state-record (make-state-info state: x
-                                           total-cost: 0
-                                           previous-state: null
-                                           heap-index: null)))
-        (setf (aref seen x) state-record)
-        (heap-push state-record heap)))
+        (heap (make-heap (list)
+                         (lambda (a b)
+                           (<= a.cost b.cost))
+                         (set .index))))
+    (dolist (x start)
+      (let ((info (make-info state: x
+                             cost: 0
+                             prev: null
+                             index: null)))
+        (setf (aref seen x) info)
+        (heap-push info heap)))
     (do ((result null))
-        ((or result (zero? (heap-length heap)))
-         result)
+        ((or result (zero? (length heap)))
+           result)
       (let* ((info (heap-pop heap))
-             (state (state-info-state info)))
+             (state info.state))
         (if (funcall goal state)
             (progn
               (setf result (list))
-              (do ((x state (state-info-previous-state (aref seen x))))
+              (do ((x state (aref seen x).prev))
                   ((null? x) (nreverse result))
                 (push x result)))
-            (dolist (nx (funcall next state))
-              (let ((nh (first nx))
-                    (cost (second nx)))
-                (let ((nh-info (aref seen nh))
-                      (tc (+ (state-info-total-cost info) cost)))
-                  (if nh-info
-                      (when (< tc (state-info-total-cost nh-info))
-                        (setf (state-info-total-cost nh-info) tc)
-                        (if (state-info-heap-index nh-info)
-                            (heap-fix heap (state-info-heap-index nh-info))
-                            (heap-push nh-info heap)))
-                      (heap-push (setf (aref seen nh)
-                                       (make-state-info state: nh
-                                                        total-cost: tc
-                                                        previous-state: state
-                                                        heap-index: null))
-                                 heap))))))))))
+            (dolist ((nh cost) (funcall next state))
+              (let ((nh-info (aref seen nh))
+                    (tc (+ info.cost cost)))
+                (if nh-info
+                    (when (< tc nh-info.cost)
+                      (setf nh-info.cost tc)
+                      (setf nh-info.prev state)
+                      (if (not (null? nh-info.index))
+                          (heap-fix heap nh-info.index)
+                          (heap-push nh-info heap)))
+                    (heap-push (setf (aref seen nh)
+                                     (make-info state: nh
+                                                cost: tc
+                                                prev: state
+                                                index: null))
+                               heap)))))))))
 
-(let* ((maze (list "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-                   "x                               x"
-                   "x  xx      xx    xxxxx  xxxxx   x"
-                   "x  xx      xx  xxx      xx  xx  x"
-                   "x  xx      xx   xxxxx   xxxxx   x"
-                   "x  xx      xx      xxx  xx      x"
-                   "x  xxxxxx  xx  xxxxx    xx       "
-                   "x                               x"
-                   "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"))
-       (start (list 1 1))
-       (next (lambda ((x y))
-               (let ((moves (list)))
-                 (dolist (xx (list (1- x) x (1+ x)))
-                   (dolist (yy (list (1- y) y (1+ y)))
-                     (when (and (or (/= xx x) (/= yy y))
-                                (<= 0 yy (1- (length maze)))
-                                (<= 0 xx (1- (length (aref maze yy))))
-                              (= " " (aref (aref maze yy) xx)))
-                       (push (list (list xx yy)
-                                   (if (or (= xx x) (= yy y))
-                                       100 141))
-                             moves))))
-                 moves)))
-       (goal (lambda ((x y))
-               (or (= x 0)
-                   (= y 0)
-                   (= y (1- (length maze)))
-                   (= x (1- (length (aref maze 0))))))))
-  (let ((sol (find-path (list start)
-                        next
-                        goal))
-        (elapsed (/ 100 (time (dotimes (i 100)
-                                (find-path (list start)
-                                           next
-                                           goal))))))
-    (when sol
-      (dolist (p sol)
-        (let ((x (first p))
-              (y (second p)))
-          (setf (aref maze y)
-                (+ (slice (aref maze y) 0 x)
-                   "+"
-                   (slice (aref maze y) (1+ x))))))
-      (dolist (L maze)
-        (display L)))
-    elapsed))
+(defun module-test ()
+  (let* ((maze (list "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+                     "x  xx                x          x"
+                     "x  xx      xx    xxxxx  xxxxx   x"
+                     "x  xx      xx  xxx      xx  xx  x"
+                     "x  xx      xx   xxxxx   xxxxx   x"
+                     "x  xx      xx      xxx  xx      x"
+                     "x  xxxxxx  xx  xxxxx    xx       "
+                     "x          xx           xx      x"
+                     "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"))
+         (h (length maze))
+         (w (length (first maze)))
+         (start (list 1 1))
+         (next (lambda ((x y))
+                 (filter (lambda (move)
+                           (let (((xx yy) (first move)))
+                             (and (< -1 yy h) (= " " (aref maze yy xx)))))
+                         `(((,(1- x) ,y) 100)
+                           ((,(1+ x) ,y) 100)
+                           ((,x ,(1- y)) 100)
+                           ((,x ,(1+ y)) 100)
+                           ((,(1- x) ,(1- y)) 141)
+                           ((,(1+ x) ,(1- y)) 141)
+                           ((,(1- x) ,(1+ y)) 141)
+                           ((,(1+ x) ,(1+ y)) 141)))))
+         (goal (lambda ((x y))
+                 (or (= x 0) (= y 0) (= y (1- h)) (= x (1- w))))))
+    (let ((sol (find-path (list start)
+                          next
+                          goal))
+          (elapsed (/ 100 (time (repeat 100
+                                        (find-path (list start)
+                                                   next
+                                                   goal))))))
+      (when sol
+        (dolist (p sol)
+          (let ((x (first p))
+                (y (second p)))
+            (setf (aref maze y)
+                  (+ (slice (aref maze y) 0 x)
+                     "+"
+                     (slice (aref maze y) (1+ x))))))
+        (dolist (L maze)
+          (display L)))
+      (display elapsed))))
+
+(export find-path)
