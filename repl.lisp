fe73362e549f1fde9a521cc969e76f4896c2a999
Andrea Griffini <agriff@tin.it>
2012-11-09 00:05:59 +0100

    wip ide

--- repl.lisp
+++ repl.lisp
@@ -6,37 +6,32 @@
 
 (defvar *ilisp*)
 
-(defun src-window (name content ilisp)
-  (let** ((w (window 0 0
-                     (floor (* (screen-width) 0.60))
-                     (- (screen-height) 4)
-                     title: name))
-          (editor (add-widget w (editor name content mode))))
-    (setf (layout w) (V border: 8 (dom editor)))
-    (setf mode.exec
-          (lambda (lines row col)
-            (let ((m (mode.parmatch lines row col)))
-              (when m
-                (let (((row0 col0) m)
-                      (txt ""))
-                  (if (= row row0)
-                      (incf txt (slice (aref lines row).text col0 (- col col0)))
-                      (progn
-                        (incf txt (+ (slice (aref lines row0).text col0) "\n"))
-                        (dolist (r (range (1+ row0) row))
-                          (incf txt (+ (aref lines r).text "\n")))
-                        (incf txt (slice (aref lines row).text 0 col))))
-                  (ilisp.send "lisp" txt))))))
-    (show-window w)))
+(defun src-tab (name content)
+  (let ((editor (editor name content mode)))
+    (setf editor.ilisp-exec
+          (lambda ()
+            (let ((lines (editor.lines))
+                  ((row col s-row s-col) (editor.pos)))
+              (declare (ignorable s-row s-col))
+              (let ((m (mode.parmatch lines row col)))
+                (when m
+                  (let (((row0 col0) m)
+                        (txt ""))
+                    (if (= row row0)
+                        (incf txt (slice (aref lines row).text col0 (- col col0)))
+                        (progn
+                          (incf txt (+ (slice (aref lines row0).text col0) "\n"))
+                          (dolist (r (range (1+ row0) row))
+                            (incf txt (+ (aref lines r).text "\n")))
+                          (incf txt (slice (aref lines row).text 0 col))))
+                    (*ilisp*.send "lisp" txt)))))))
+    editor))
 
 (defun inferior-lisp ()
-  (let** ((w (window (+ 4 (floor (* (screen-width) 0.60))) 0
-                     (- (floor (* (screen-width) 0.40)) 8)
-                     (- (screen-height) 4)
-                     title: "Inferior Lisp"))
-          (inspect (add-widget w (button "Inspect" #'inspect)))
-          (reset (add-widget w (button "Reset" #'reset)))
-          (clear (add-widget w (button "Clear" #'clear)))
+  (let** ((container (create-element "div"))
+          (inspect (append-child container (button "Inspect" #'inspect)))
+          (reset (append-child container (button "Reset" #'reset)))
+          (clear (append-child container (button "Clear" #'clear)))
           (ilisp (ilisp:new #'reply))
           (#'inspect ()
                      (mode.inspect-ilisp ilisp))
@@ -51,29 +46,62 @@
                    (ilisp.reset))
           (#'clear ()
                    (ilisp.send "javascript"
-                               "repl.value=\"\"")))
-    (add-widget w ilisp.iframe)
+                               "repl.value=\"\""))
+          (layout (V border: 16 spacing: 16
+                     (dom ilisp.iframe)
+                     size: 30
+                     (H :filler:
+                        size: 80
+                        (dom reset)
+                        (dom clear)
+                        (dom inspect)
+                        :filler:))))
+    (append-child container ilisp.iframe)
     (set-style ilisp.iframe
+               position "absolute"
                border "solid 1px #CCCCCC"
                px/padding 0
                px/margin -1
                opacity 1)
-    (setf (layout w) (V border: 16 spacing: 16
-                        (dom ilisp.iframe)
-                        size: 30
-                        (H :filler:
-                           size: 80
-                           (dom reset)
-                           (dom clear)
-                           (dom inspect)
-                           :filler:)))
-    (show-window w)
-    ilisp))
+    (setf container."data-resize"
+          (lambda (x0 y0 x1 y1)
+            (set-coords layout 0 0 (- x1 x0) (- y1 y0))))
+    (setf container.ilisp ilisp)
+    container))
 
 (defun main ()
-  (let ((ilisp (inferior-lisp)))
-    (src-window "test.lisp"
-                ""
-                ilisp)))
+  (let** ((w (window 0 0 (- (screen-width) 4) (- (screen-height) 4)
+                     title: "JsLisp IDE"))
+          (sources (add-widget w (tabbed (list "test.lisp"))))
+          (ilisp (add-widget w (inferior-lisp))))
+    (setf *ilisp* ilisp.ilisp)
+
+    (let** ((pg (aref (tab-pages sources) 0))
+            (editor (append-child pg (set-style (src-tab "test.lisp" "")
+                                                position "absolute")))
+            (layout (V border: 8 spacing: 8 (dom editor))))
+      (setf (aref (tab-pages sources) 0)."data-resize"
+            (lambda (x0 y0 x1 y1)
+              (set-coords layout 0 0 (- x1 x0) (- y1 y0))))
+
+      (document.body.addEventListener
+       "keydown"
+       (lambda (event)
+         (let ((stop true))
+           (cond
+             ((and event.ctrlKey (= event.which 73))
+              (mode.inspect-ilisp *ilisp*))
+             ((and event.ctrlKey (= event.which 13))
+              (editor.ilisp-exec))
+             (true (setf stop false)))
+           (when stop
+             (event.stopPropagation)
+             (event.preventDefault))))
+       true))
+    (set-layout w (V border: 8 spacing: 8
+                     (dom sources)
+                     weight: 25
+                     (dom ilisp)))
+    (show-window w center: true)))
 
 (main)
