1c4e5a4a9ffa15c062cee2ca0693cf5f3ea5a384
Andrea Griffini <agriff@tin.it>
2012-11-13 22:33:29 +0100

    simplified ttt example

--- examples/ttt.lisp
+++ examples/ttt.lisp
@@ -3,37 +3,27 @@
 
 (defun ttt ()
   (let** ((w (window 0 0 400 400 title: "T-T-T"))
-          (board (map (lambda (i)
-                        (map (lambda (j)
-                               (cell i j))
-                             (range 3)))
-                      (range 3)))
           (color 0)
-          (markers "OX")
-          (grid (add-widget w (table board)))
-          (#'cell (i j)
-            (declare (ignorable i j))
-            (let ((div (create-element "div")))
-              (set-style div
-                         position "absolute"
-                         textAlign "center"
-                         backgroundColor "#FFFFFF")
+          (#'cell()
+            (let ((div (set-style (create-element "div")
+                                  position "absolute"
+                                  textAlign "center"
+                                  backgroundColor "#EEEEEE")))
               (setf div."data-resize"
                     (lambda (x0 y0 x1 y1)
                       (declare (ignorable x0 x1))
                       (setf div.style.fontSize
                             (+ (* (- y1 y0) 0.9) "px"))))
-              (setf div.onclick
-                    (lambda (event)
-                      (declare (ignorable event))
-                      (when (= div.textContent "")
-                        (setf div.textContent
-                              (aref markers color))
-                        (setf color (- 1 color)))))
-              div)))
-    (set-layout w (V border: 16 spacing: 8 (dom grid)))
-    (show-window w center: true)
-    w))
+              (set-handler div onclick
+                (when (= div.textContent "")
+                  (setf div.textContent (aref "OX" color))
+                  (setf color (- 1 color))))
+              (add-widget w div))))
+    (set-layout w (V border: 16 spacing: 8
+                     (H (dom (cell)) (dom (cell)) (dom (cell)))
+                     (H (dom (cell)) (dom (cell)) (dom (cell)))
+                     (H (dom (cell)) (dom (cell)) (dom (cell)))))
+    (show-window w center: true)))
 
 (defun main ()
   (ttt))
