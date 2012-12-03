c390e2cfad0cd5093459c48682b387b9552fce19
Andrea Griffini <agriff@tin.it>
2012-12-03 23:40:14 +0100

    browser example

---
+++ examples/browser.lisp
@@ -0,0 +1,29 @@
+(import * from gui)
+(import * from layout)
+
+(defun browser ()
+  (let** ((w (window 0 0 0.75 0.75 title: "Browser"))
+          (address (add-widget w (input "address")))
+          (go (add-widget w (button "go" #'go)))
+          (page (add-widget w (create-element "iframe")))
+          (#'go ()
+            (setf page.src (text address))))
+    (set-layout w (V border: 8 spacing: 8
+                     size: 40
+                     (H (dom address)
+                        size: 60
+                        (V :filler:
+                           size: 25
+                           (dom go)))
+                     size: undefined
+                     (dom page)))
+    (setf address.lastChild.onkeydown
+          (lambda (event)
+            (when (= event.which 13)
+              (go))))
+    (show-window w center: true)))
+
+(defun main ()
+  (browser))
+
+(main)
