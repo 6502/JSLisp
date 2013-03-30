a294c14adf2243b21e71398b408c68e65dfa5278
Andrea Griffini <agriff@tin.it>
2013-03-30 18:26:46 +0100

    ignore ctrl-r (page reload)

--- editor.lisp
+++ editor.lisp
@@ -738,6 +738,10 @@
       (set-handler hinput onkeydown
                    (let ((block true))
                      (case event.which
+                           (#.(char-code "R")
+                              ;; Ignore
+                              (unless event.ctrlKey
+                                (setf block false)))
                            (#.(char-code "F")
                               (if event.ctrlKey
                                   (setf fps (not fps))
