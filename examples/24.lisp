9524d1723252b80d195ef6708b0d1e4f7f900870
Andrea Griffini <agriff@tin.it>
2013-10-06 12:32:50 +0200

    wip 24

--- examples/24.lisp
+++ examples/24.lisp
@@ -129,31 +129,82 @@
   (funcall e.f (value-of e.a) (value-of e.b)))
 
 (defun game ()
-  (let** ((container (append-child document.body
+  (let** ((digits (list 1 2 3 4))
+          (target 10)
+          (timerstart (clock))
+          (elapsed 0)
+          (score 0)
+          (running false)
+          (count-down 5)
+
+          (container (append-child document.body
                                    (set-style (create-element "div")
                                               position "absolute"
                                               px/left 0
                                               px/top 0
                                               px/right 0
                                               px/bottom 0)))
-          (digits (repeat-collect 4 (random-choice (range 1 10))))
-          (target (do ((t 0))
-                    ((<= 1 t 99) t)
-                    (setf t (do ((r (slice digits)))
-                              ((= (length r) 1) (first r))
-                              (let** ((a (random-choice r))
-                                      (b (random-choice (remove-first a r)))
-                                      (x (funcall (random-choice (list #'+ #'- #'* #'/)) a b)))
-                                (when (< (abs (- x (round x))) 0.0001)
-                                  (nremove-first a r)
-                                  (nremove-first b r)
-                                  (push x r)))))))
-          (canvas (append-child container (set-style (create-element "canvas")
-                                                     position "absolute"
-                                                     px/left 0
-                                                     px/top 0)))
+          (#'restart ()
+            (setf digits (repeat-collect 4 (random-choice (range 1 10))))
+            (setf target (do ((t 0))
+                           ((<= 1 t 99) t)
+                           (setf t (do ((r (slice digits)))
+                                     ((= (length r) 1) (first r))
+                                     (let** ((a (random-choice r))
+                                             (b (random-choice (remove-first a r)))
+                                             (x (funcall (random-choice (list #'+ #'- #'* #'/)) a b)))
+                                       (when (< (abs (- x (round x))) 0.0001)
+                                         (nremove-first a r)
+                                         (nremove-first b r)
+                                         (push x r)))))))
+            (let ((i 0))
+              (dolist (e (splice entities))
+                (when (digit? e)
+                  (setf e.n (aref digits i))
+                  (setf e.x (* (+ i 2) R2))
+                  (setf e.y (* R2 2))
+                  (incf i))
+                (when (or (digit? e) e.entities)
+                  (push e entities))))
+            (setf count-down 5)
+            (setf timerstart (clock))
+            (repaint))
+          (canvas (append-child container
+                                (set-style (create-element "canvas")
+                                           position "absolute"
+                                           px/left 0
+                                           px/top 0)))
           (ctx (canvas.getContext "2d"))
           (entities (list))
+          (timerdiv (append-child container
+                                  (set-style (create-element "div")
+                                             position "absolute"
+                                             px/right 0
+                                             px/bottom 0
+                                             px/fontSize R
+                                             fontWeight "bold"
+                                             fontFamily "monospace"
+                                             color "#888")))
+          (scorediv (append-child container
+                                  (set-style (create-element "div")
+                                             position "absolute"
+                                             px/bottom 0
+                                             px/left 0
+                                             px/fontSize R
+                                             fontWeight "bold"
+                                             fontFamily "monospace"
+                                             color "#008")))
+          (#'add-score (x)
+            (incf score x)
+            (setf scorediv.textContent ~"Score: {score}")
+            (setf timerdiv.textContent "")
+            (set-style scorediv
+                       display "none")
+            (set-timeout #'restart 1000))
+          (#'update-timer ()
+            (when running
+              (setf elapsed (- (clock) timerstart))
+              (setf timerdiv.textContent ~"{(to-fixed (/ elapsed 1000) 3)}")))
           (#'repaint ()
             (set-style canvas
                        px/width container.offsetWidth
@@ -169,48 +220,54 @@
               (setf ctx.fillStyle "#FFF")
               (setf ctx.font ~"bold {(* R2 3.5)}px monospace")
               (setf ctx.textAlign "center")
-              (ctx.fillText (+ "" target) (* R2 3.5) (* R2 6.5))
+              (ctx.fillText (+ "" (or count-down target))
+                            (* R2 3.5)
+                            (* R2 (if count-down 5.5 6.5)))
+              (unless count-down
+                (setf ctx.strokeStyle "#888")
+                (setf ctx.lineWidth 1)
+                (ctx.beginPath)
+                (ctx.moveTo (* R2 0.5) (* R2 1.5))
+                (ctx.lineTo (* R2 6.5) (* R2 1.5))
+                (ctx.lineTo (* R2 6.5) (* R2 7.5))
+                (ctx.lineTo (* R2 0.5) (* R2 7.5))
+                (ctx.closePath)
+                (ctx.stroke)
 
-              (setf ctx.strokeStyle "#888")
-              (setf ctx.lineWidth 1)
-              (ctx.beginPath)
-              (ctx.moveTo (* R2 0.5) (* R2 1.5))
-              (ctx.lineTo (* R2 6.5) (* R2 1.5))
-              (ctx.lineTo (* R2 6.5) (* R2 7.5))
-              (ctx.lineTo (* R2 0.5) (* R2 7.5))
-              (ctx.closePath)
-              (ctx.stroke)
-
-              (dolist (e entities)
-                (draw-links ctx e))
-              (dolist (e (reverse entities))
-                (draw ctx e))
-              (when (= (length result) 1)
-                (let** ((x0 w)
-                        (y0 h)
-                        (x1 0)
-                        (y1 0)
-                        (#'visit (e)
-                          (when (< e.x x0) (setf x0 e.x))
-                          (when (> e.x x1) (setf x1 e.x))
-                          (when (< e.y y0) (setf y0 e.y))
-                          (when (> e.y y1) (setf y1 e.y))
-                          (when e.a (visit e.a))
-                          (when e.b (visit e.b))))
-                  (visit (first result))
-                  (setf ctx.strokeStyle
-                        (if (< (abs (- (value-of (first result)) target))
-                               0.00001)
-                            "#0F0"
-                            "#F00"))
-                  (setf ctx.lineWidth 6)
-                  (ctx.beginPath)
-                  (ctx.moveTo (- x0 R2) (- y0 R2))
-                  (ctx.lineTo (+ x1 R2) (- y0 R2))
-                  (ctx.lineTo (+ x1 R2) (+ y1 R2))
-                  (ctx.lineTo (- x0 R2) (+ y1 R2))
-                  (ctx.closePath)
-                  (ctx.stroke)))))
+                (dolist (e entities)
+                  (draw-links ctx e))
+                (dolist (e (reverse entities))
+                  (draw ctx e))
+                (when (= (length result) 1)
+                  (let** ((x0 w)
+                          (y0 h)
+                          (x1 0)
+                          (y1 0)
+                          (#'visit (e)
+                            (when (< e.x x0) (setf x0 e.x))
+                            (when (> e.x x1) (setf x1 e.x))
+                            (when (< e.y y0) (setf y0 e.y))
+                            (when (> e.y y1) (setf y1 e.y))
+                            (when e.a (visit e.a))
+                            (when e.b (visit e.b))))
+                    (visit (first result))
+                    (setf ctx.strokeStyle
+                          (if (< (abs (- (value-of (first result)) target))
+                                 0.00001)
+                              (progn
+                                (when running
+                                  (setf running false)
+                                  (add-score (floor (/ (* 100 11000) (+ 1000 elapsed)))))
+                                "#0F0")
+                              "#F00"))
+                    (setf ctx.lineWidth 6)
+                    (ctx.beginPath)
+                    (ctx.moveTo (- x0 R2) (- y0 R2))
+                    (ctx.lineTo (+ x1 R2) (- y0 R2))
+                    (ctx.lineTo (+ x1 R2) (+ y1 R2))
+                    (ctx.lineTo (- x0 R2) (+ y1 R2))
+                    (ctx.closePath)
+                    (ctx.stroke))))))
           (#'down (x y)
             (let* (((x0 y0) (element-pos canvas))
                    (cb (any (e entities) (hit e (- x x0) (- y y0)))))
@@ -239,10 +296,19 @@
       (push (new-digit x (* R2 2) n) entities))
 
     (set-interval (lambda ()
+                    (update-timer)
                     (when (or (/= canvas.width container.offsetWidth)
-                              (/= canvas.height container.offsetHeight))
+                              (/= canvas.height container.offsetHeight)
+                              (and count-down (> (clock) (+ 1000 timerstart))))
+                      (when count-down
+                        (decf count-down)
+                        (setf timerstart (clock))
+                        (unless count-down
+                          (set-style scorediv
+                                     display "block")
+                          (setf running true)))
                       (repaint)))
-                  50)
+                  53)
     (set-handler canvas onmousedown
       (event.preventDefault)
       (event.stopPropagation)
